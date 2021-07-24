d08c270835a943253acc0187976c2c754ce6f7c0
vishalgupta07 <gvishal.0862@gmail.com>
2021-07-24 15:36:55 +0530

    Initial commit

---
+++ src/test/java/workspace/vishalgupta07/springboottemplate/SpringBootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package workspace.vishalgupta07.springboottemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
